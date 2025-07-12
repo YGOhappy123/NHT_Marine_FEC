a76b370d61a5a6c5002c4a4b3c123f5a0079c4df
ygohappy123 <hagiahuy21052003@gmail.com>
2025-07-13 00:46:36 +0700

    add authentication page

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,30 +1,38 @@
-// type SocialLink = {
-//     platform: string
-//     url: string
-//     icon: IconProp
-// }
+import { SiFacebook, SiYoutube, SiTiktok, SiInstagram, SiX, IconType } from '@icons-pack/react-simple-icons'
 
-// type NavigationTab = {
-//     label: string
-//     href: string
-//     roles?: IRole[]
-// }
+type NavigationTab = {
+    label: string
+    href: string
+    authRequired?: boolean
+}
 
-// export const NAVIGATION_TABS: NavigationTab[] = [
-//     { label: 'trang chủ', href: '/' },
-//     { label: 'dịch vụ', href: '/our-services' },
-//     { label: 'mua vé', href: '/buy-tickets' },
-//     { label: 'quản lý', href: '/dashboard', roles: ['admin', 'staff'] }
-// ]
+export const NAVIGATION_TABS: NavigationTab[] = [
+    { label: 'trang chủ', href: '/' },
+    { label: 'giới thiệu', href: '/about-us' },
+    { label: 'sản phẩm', href: '/products' },
+    { label: 'tư vấn', href: '/advisory' },
+    { label: 'giỏ hàng', href: '/cart', authRequired: true },
+    { label: 'profile', href: '/profile', authRequired: true }
+]
 
-// export const SOCIAL_LINKS: SocialLink[] = [
-//     { platform: 'facebook', url: 'https://www.facebook.com', icon: faFacebook },
-//     { platform: 'youtube', url: 'https://youtube.com', icon: faYoutube },
-//     { platform: 'tiktok', url: 'https://www.tiktok.com', icon: faTiktok },
-//     { platform: 'instagram', url: 'https://www.instagram.com', icon: faInstagram },
-//     { platform: 'x', url: 'https://x.com', icon: faTwitter }
-// ]
+type SocialLink = {
+    platform: string
+    url: string
+    Icon: IconType
+}
+
+export const SOCIAL_LINKS: SocialLink[] = [
+    { platform: 'facebook', url: 'https://www.facebook.com', Icon: SiFacebook },
+    { platform: 'youtube', url: 'https://youtube.com', Icon: SiYoutube },
+    { platform: 'tiktok', url: 'https://www.tiktok.com', Icon: SiTiktok },
+    { platform: 'instagram', url: 'https://www.instagram.com', Icon: SiInstagram },
+    { platform: 'x', url: 'https://x.com', Icon: SiX }
+]
 
 export const LOGIN_SESSION_EXPIRED_MESSAGE = 'Phiên đăng nhập hết hạn. Xin vui lòng đăng nhập lại.'
 
 export const INTRODUCTION_VIDEO_URL = 'https://youtube.com'
+
+export const AUTH_CAROUSEL_IMAGES = [
+    'https://www.itl.cat/pngfile/big/185-1857360_animated-gif-fish-share-or-download-water-with.gif'
+]
